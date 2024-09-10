d98122679872395f1ab667f218f0c48f3f7b40c7
Anton <80984465+Antongo22@users.noreply.github.com>
2024-09-10 18:48:58 +0300

    кт 3

--- Program.cs
+++ Program.cs
@@ -1,64 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Faker.Resources;
+using Faker.Extensions;
 
 namespace learningcsh
 {
-    public class MyClass
+    class User
     {
-        public int Value { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
 
-        public MyClass(int value)
+        public User(string name, int age)
         {
-            Value = value;
-        }
-
-        public static bool operator ==(MyClass a, MyClass b)
-        {
-            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
-                return true;
-
-            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
-                return false;
-
-            return a.Value == b.Value;
-        }
-
-        public static bool operator !=(MyClass a, MyClass b)
-        {
-            return !(a == b);
-        }
-
-        public static bool operator true(MyClass obj)
-        {
-            return obj.Value > 0;
-        }
-
-        public static bool operator false(MyClass obj)
-        {
-            return obj.Value <= 0;
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (obj is MyClass other)
-            {
-                return this == other;
-            }
-            return false;
-        }
-
-        public override int GetHashCode()
-        {
-            return Value.GetHashCode();
-        }
-
-        public static explicit operator int(MyClass obj)
-        {
-            return obj.Value;
+            Name = name;
+            Age = age;
         }
     }
 
@@ -66,32 +26,143 @@ namespace learningcsh
     internal class Program
     {
 
-        static void Main(string[] args)
+        
+
+        static void DB()
         {
-            MyClass obj1 = new MyClass(10);
-            MyClass obj2 = new MyClass(10);
-            MyClass obj3 = new MyClass(-5);
+            string connectionString = "Data Source=users.db;Version=3;";
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
 
-            Console.WriteLine(obj1 == obj2);  
-            Console.WriteLine(obj1 != obj3);  
+                string createTableQuery = @"
+                CREATE TABLE IF NOT EXISTS Users (
+                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Name TEXT NOT NULL,
+                    Age INTEGER NOT NULL
+                );";
+                using (var createTableCommand = new SQLiteCommand(createTableQuery, connection))
+                {
+                    createTableCommand.ExecuteNonQuery();
+                }
 
-            if (obj1)
-            {
-                Console.WriteLine("obj1 is true");
-            }
-            else
-            {
-                Console.WriteLine("obj1 is false");
-            }
 
-            if (obj3)
-            {
-                Console.WriteLine("obj3 is true");
+                for (int i = 0; i < 10; i++)
+                {
+                    var name = Faker.Name.First();
+                    var age = Faker.RandomNumber.Next(1, 70);
+
+                    User user = new User(name, age);
+
+                    try
+                    {
+                        if (age < 14)
+                        {
+                            throw new Exception("Возраст пользователя должен быть не менее 14 лет1");
+                        }
+
+                        string insertQuery = "INSERT INTO Users (Name, Age) VALUES (@Name, @Age)";
+                        using (var insertCommand = new SQLiteCommand(insertQuery, connection))
+                        {
+                            insertCommand.Parameters.AddWithValue("@Name", name);
+                            insertCommand.Parameters.AddWithValue("@Age", age);
+                            insertCommand.ExecuteNonQuery();
+                        }
+
+                        Console.WriteLine($"Пользователь {name}, возраст {age} добавлен в базу данных.");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Пользователь {name}, возраст {age} не был добавлен в базу данных из-за возраста.");
+                    }
+
+
+                }
+
+                connection.Close();
             }
-            else
+        }
+
+        static void PrintUsers()
+        {
+            string connectionString = "Data Source=users.db;Version=3;";
+            using (var connection = new SQLiteConnection(connectionString))
             {
-                Console.WriteLine("obj3 is false");
+                connection.Open();
+
+                string selectQuery = "SELECT Id, Name, Age FROM Users";
+                using (var selectCommand = new SQLiteCommand(selectQuery, connection))
+                {
+                    using (var reader = selectCommand.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            int id = reader.GetInt32(0);
+                            string name = reader.GetString(1);
+                            int age = reader.GetInt32(2);
+
+                            Console.WriteLine($"ID: {id}, Name: {name}, Age: {age}");
+                        }
+                    }
+                }
+
+                connection.Close();
             }
+        }
+
+        static void Create()
+        {
+            string connectionString = "Data Source=users.db;Version=3;";
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                string createTableQuery = @"
+                CREATE TABLE IF NOT EXISTS Users (
+                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Name TEXT NOT NULL,
+                    Age INTEGER NOT NULL
+                );";
+                using (var createTableCommand = new SQLiteCommand(createTableQuery, connection))
+                {
+                    createTableCommand.ExecuteNonQuery();
+                }
+
+                connection.Close();
+            }
+        }
+
+        static void Main()
+        {
+
+            Create();
+
+            do
+            {
+                Console.WriteLine("Введите команду: n - добавить пользователя, s - вывести список пользователей, 0 - выход из программы");
+                Console.Write("> ");
+                string input = Console.ReadLine();
+                if (input == "n")
+                {
+                    DB();
+                }
+                else if (input == "s")
+                {
+                    PrintUsers();
+                }
+                else if (input == "0")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Повторите команду ещё раз!");
+                }
+
+                Console.WriteLine("\n\n");
+            } while (true);
+
+            Console.WriteLine("Пока!");
         }
     }
 }
